77661aec041381f220bb5ba52ca8b6497c9352b8
davy1ex <davy1ex@pop-os.localdomain>
2020-05-16 18:50:25 +0400

    redesigne

--- index.php
+++ index.php
@@ -7,20 +7,22 @@
 
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/index.css">
+
+    
 </head>
 <body>
-    <div class="container">
-        <div class="whoami">
+    <div class="container mynameis">
+        <div class="mynameis_wrapper">
             <div class="avatar">
                 <img src="/img/avatar.jpg" alt="">
             </div>
             <!-- <div class="title">davy1ex</div> -->
-            <div class="mynameis">Давыдов Алексей</div>
+            <div class="myname">Давыдов Алексей</div>
             <div class="profession">Веб разработчик</div>
         </div>
     </div>
 
-    <header>
+    <header id="header">
         <div class="header_wrapper">        
             <a href="#"><div class="header_item active">Главная</div></a>
             <a href="#"><div class="header_item">Обо мне</div></a>
@@ -33,7 +35,34 @@
 
     </header>
 
+    <div class="container content desc">
+        <div class="title">
+            <span class="line">Обо мне</span>
+            <!-- <div class="line"></div> -->
+        </div>
+
+        <div class="text">
+            Вы находитесь на моей странице портфолио. Тут можно найти информацию о моих услугах, опыте и проектах.
+        </div>
+
+        <div class="text">
+            <br><strong>Специализация</strong>: Программирование сайтов-визиток / лендингов / сайтов под ключ / Web-сервисов. Адаптивная вёрстка. <br> <br>
+
+            <strong>Опыт работы</strong>: более 2 лет. <br> <br>
+
+            <strong>Используемые технологии</strong>: HTML, CSS, JavaScript (Vue.js, Node.js), PHP (Symfony), MySQL, AJAX, Python (Django, Flask).
+        </div>
+    </div>
+
+    <div class="container content">
+    SKILLS
+    </div>
+
+    <div class="container">
+    FOOTER
+    </div>
 
 
+    <script src="js/index.js"></script>
 </body>
 </html>
