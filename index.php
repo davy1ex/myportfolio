f10c1ced5bac74850b008898f8d462532ab015e7
dezy1ex <dezy1ex@gmail.com>
2020-05-16 14:56:37 +0400

    initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>davy1ex</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="container">
+        <div class="whoami">
+            <div class="avatar">
+                <img src="/img/avatar.jpg" alt="">
+            </div>
+            <!-- <div class="title">davy1ex</div> -->
+            <div class="mynameis">Давыдов Алексей</div>
+            <div class="profession">Веб разработчик</div>
+        </div>
+    </div>
+
+    <header>
+        <div class="header_wrapper">        
+            <a href="#"><div class="header_item active">Главная</div></a>
+            <a href="#"><div class="header_item">Обо мне</div></a>
+            <a href="#"><div class="header_item">Резюме</div></a>
+            <a href="#"><div class="header_item">Услуги</div></a>
+            <a href="#"><div class="header_item">Портфолио</div></a>
+            <a href="#"><div class="header_item">Контакты</div></a>
+        </div>
+
+
+    </header>
+
+
+
+</body>
+</html>
