7a62813b1831cf5a67247ffad2634ae9266c4fef
dezy1ex <dezy1ex@gmail.com>
2020-05-25 22:59:43 +0400

    added add projects from admin

---
+++ admin/add_project.php
@@ -0,0 +1,20 @@
+<?php 
+include("../configDB.php");
+
+$title = $_GET['title'];
+$photo = $_GET['photo'];
+$description = $_GET['description'];
+$github = $_GET['github'];
+
+$sql = "INSERT INTO `projects` (`title`, `photo`, `description`, `github`) 
+VALUES ('$title', '$photo', '$description', '$github')";
+// echo $sql;
+$result = mysqli_query($link, $sql);
+
+if ($result) {
+    header("Location: ./admin.php");
+}
+
+else {
+    echo "ERROR";
+}
--- admin/remove_project.php
+++ admin/remove_project.php
@@ -1,8 +1,6 @@
 <?php 
 include("../configDB.php");
 
-// echo "gg" . $_GET['project_id'] . "!";
-
 $sql = "DELETE FROM `projects` WHERE `id`=" . "'" . (int)$_GET['project_id'] . "'" ;
 // echo $sql;
 $result = mysqli_query($link, $sql);
